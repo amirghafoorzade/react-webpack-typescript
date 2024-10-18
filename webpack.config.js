dc9d45cec9e7748f1060bdb1efe1dc3afd2984e5
Amir Ghafoorzade <amirghafoorzade@gmail.com>
2024-10-18 18:22:27 +0330

    adding css loader and mini css extract plugin for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   mode: "production",
-  enode_modulesntry: "./src/index.ts",
+  entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -18,11 +22,25 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
     ],
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-  ],
+  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
 };
